a611643d48932b688f192897dbf359b1afde0860
sam9116 <sam.b5687@gmail.com>
2017-03-14 15:31:03 -0600

    start recording errors

--- django_azure/settings.py
+++ django_azure/settings.py
@@ -123,3 +123,29 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+
+
+
+
+LOGGING = {
+ 'version': 1,
+ 'disable_existing_loggers': False,
+ 'filters': {
+ 'require_debug_false': {
+ '()': 'django.utils.log.RequireDebugFalse'
+ }
+ },
+ 'handlers': {
+ 'logfile': {
+ 'class': 'logging.handlers.WatchedFileHandler',
+ 'filename': 'D:\home\site\wwwroot\myapp.log'
+ }
+ },
+ 'loggers': {
+ 'django': {
+ 'handlers': ['logfile'],
+ 'level': 'ERROR',
+ 'propagate': False,
+ }
+ }
+ }
